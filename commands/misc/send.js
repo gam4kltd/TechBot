10aa9d3005a4b273de7c9b61c44e6e2c5487ecf4
UncautiousGamer <gam4kltd@gmail.com>
2021-11-17 16:12:28 -0700

    dropdown menu roles

---
+++ commands/misc/send.js
@@ -0,0 +1,39 @@
+const { Message } = require("discord.js");
+
+module.exports = {
+    category: 'Configuration',
+    description: 'Sends a message.',
+    
+    permissions: ['ADMINISTRATOR'], 
+
+    minArgs: 2,
+    expectedArgs: '<channel> <text>',
+    expectedArgsTypes: ['CHANNEL', 'STRING'],
+
+    slash: 'both',
+    testOnly: true,
+    guildOnly: true,
+
+    callback: ({ message, interaction, args }) => {
+        const channel = message ? message.mentions.channels.first() : interaction.options.getChannel('channel')
+        if (!channel || channel.type !== 'GUILD_TEXT') {
+            return {
+                custom: true,
+                content: 'Please tag a text channel.',
+                ephemeral: true
+            }
+        }
+
+        args.shift() // Remove the channel from the args array
+        const text = args.join(' ')
+
+        channel.send(text)
+
+        if (interaction) {
+            interaction.reply({
+                content: 'Sent message.',
+                ephemeral: true
+            })
+        }
+    }
+}
