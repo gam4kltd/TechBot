dcbaf74c2d80de022647590afad00522811d1b9f
UncautiousGamer <gam4kltd@gmail.com>
2021-11-16 20:22:39 -0700

    basic moderation

---
+++ commands/moderation/kick.js
@@ -0,0 +1,41 @@
+module.exports = {
+    category: 'Moderation',
+    description: 'Kicks a user.',
+
+    requireRoles: true,
+
+    slash: 'both',
+    testOnly: true,
+
+    guildOnly: true,
+
+    minArgs: 2,
+    expectedArgs: '<user> <reason>',
+    expectedArgsTypes: ['USER', 'STRING'],
+
+    callback: ({ message, interaction, args }) => {
+        const target = message ? message.mentions.members.first() : interaction.options.getMember('user')
+        if (!target) {
+            return 'Please tag someone to kick.'
+        }
+
+        if (!target.kickable) {
+            return {
+                custom: true,
+                content: 'Cannot kick that user.',
+                ephemeral: true
+            }
+        }
+
+        args.shift()
+        const reason = args.join(' ')
+
+        target.kick(reason)
+
+        return {
+            custom: true,
+            content: `You kicked <@${target.id}>`,
+            ephemeral: true
+        }
+    }
+}
